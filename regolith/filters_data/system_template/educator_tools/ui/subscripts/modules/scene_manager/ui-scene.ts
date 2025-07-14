68973595e70d710d0f34ae8c2fa7294f25a5b7af
Fabrizio La Rosa <fabrizio.larosa@shapescape.com>
2025-07-14 15:25:11 +0200

    Fix buttonHandlers alignment for label and divider
    Added empty functions to buttonHandlers in addLabel and addDivider to maintain correct alignment with form elements. This prevents potential mismatches between form controls and their handlers.

--- regolith/filters_data/system_template/educator_tools/ui/subscripts/modules/scene_manager/ui-scene.ts
+++ regolith/filters_data/system_template/educator_tools/ui/subscripts/modules/scene_manager/ui-scene.ts
@@ -100,6 +100,7 @@ export class ActionUIScene {
 	 */
 	addLabel(label: string | RawMessage): void {
 		this.form.label(label);
+		this.buttonHandlers.push(() => {});
 	}
 
 	/**
@@ -110,6 +111,7 @@ export class ActionUIScene {
 	 */
 	addDivider(): void {
 		this.form.divider();
+		this.buttonHandlers.push(() => {});
 	}
 
 	/**
